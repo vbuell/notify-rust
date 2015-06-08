bef27d6e7f63034d4dcd974ab7368d03d889787a
Hendrik Sollich <hendrik@hoodie.de>
2015-06-08 15:45:24 +0200

    up to date with v0.0.6

--- notify_rust/sidebar-items.js
+++ notify_rust/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Notification","Desktop Notification."]],"enum":[["NotificationHint",""]],"mod":[["server","**Experimental** server taking the place of your Desktop Environments Notification Server."]],"fn":[["exe_name","Executable Name"]]});
+initSidebarItems({"fn":[["exe_name","Executable Name"]],"enum":[["NotificationHint",""]],"struct":[["Notification","Desktop Notification."]],"mod":[["server","**Experimental** server taking the place of your Desktop Environments Notification Server."]]});
