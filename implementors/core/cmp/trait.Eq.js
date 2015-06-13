5a9006456b667aea868290b747ebd416f6ee08cd
Hendrik Sollich <hendrik@hoodie.de>
2015-06-13 19:51:59 +0200

    doc for v0.0.7

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['notify_rust'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='notify_rust/enum.NotificationHint.html' title='notify_rust::NotificationHint'>NotificationHint</a>",];
+implementors['notify_rust'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='notify_rust/enum.NotificationUrgency.html' title='notify_rust::NotificationUrgency'>NotificationUrgency</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='notify_rust/enum.NotificationHint.html' title='notify_rust::NotificationHint'>NotificationHint</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['notify_rust'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='notify_rust/enum.NotificationHint.html' title='notify_rust::NotificationHint'>NotificationHint</a>",];
+implementors['notify_rust'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='notify_rust/enum.NotificationUrgency.html' title='notify_rust::NotificationUrgency'>NotificationUrgency</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='notify_rust/enum.NotificationHint.html' title='notify_rust::NotificationHint'>NotificationHint</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
