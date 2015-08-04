1eb21b785ce7b3c179a7839162a6eeecbb9d0c84
Hendrik Sollich <hendrik@hoodie.de>
2015-08-04 11:24:51 +0200

    doc for v2.0.0

---
+++ implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['notify_rust'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='notify_rust/struct.NotificationHandle.html' title='notify_rust::NotificationHandle'>NotificationHandle</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
